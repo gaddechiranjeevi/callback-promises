a0af6f9cd2a63d9b033a484df0fa5230df8ba7cb
gaddechiranjeevi <gaddechiru@gmail.com>
2022-10-15 16:03:46 +0530

    commit

---
+++ callbacks.js
@@ -0,0 +1,35 @@
+const posts=[
+    {title:'Post One',body:'This is post one',createdAt: new Date().getTime()},
+    {title:'Post Two',body:'This is post two',createdAt: new Date().getTime()}
+]
+let intervalid = 0;
+ 
+function getPosts(){
+    setTimeout(()=>{
+
+        let output='';
+
+        posts.forEach((posts,index)=>{
+
+            output+=`<li>${posts.title} - last Edited${(new Date().getTime() - posts.createdAt)/1000}seconds ago</li>`;
+
+        });
+
+        document.body.innerHTML=output;
+
+    },1000)
+}
+
+function createPost(post,callback){
+
+    setTimeout(()=>{
+
+        posts.push({ ...post,createdAt: new Date().getTime()});
+
+        callback();
+
+    },2000);
+}
+createPost({title:'Post Three',body:'This is post three'},getPosts);
+
+createPost({title:'Post Four',body:'This is post four'},getPosts);
